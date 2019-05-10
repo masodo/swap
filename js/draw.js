4295a370fe842797c48bacae154751ab6e169c52
masodo <41118685+masodo@users.noreply.github.com>
2019-05-10 15:22:14 -0400

    Modified for IBPro style scorekeeping

--- js/draw.js
+++ js/draw.js
@@ -25,8 +25,8 @@ var renderer = function() {
 		ctx.clearRect(0, 0, width, height);
 	}
 
-	var renderText = function(deaths, level, tip) {
-		hud.innerHTML = deaths + " Deaths " + " | Level " + (level+1) + " / " + levels.length + " <sup><a style='font-size: 12px; text-decoration: none;' href='#"+(level+1)+"'>(link)</a></sup>";
+	var renderText = function(deaths, level, tip, myScore) {
+		hud.innerHTML = "Score: " + myScore + " | " + deaths + " Deaths " + " | Level " + (level+1) + " / " + levels.length + " <sup><a style='font-size: 12px; text-decoration: none;' href='#"+(level+1)+"'>(link)</a></sup>";
 		tipDisplay.innerHTML =  "<em>" + tip + "</em>";
 	}
 
